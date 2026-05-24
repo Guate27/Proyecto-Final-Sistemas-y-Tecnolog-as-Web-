293c24c6b08e4dfca6a2e7028e85b6d224bf410d
Guate27 <reypellecer@gmail.com>
2026-05-24 00:44:09 -0600

    main.jsx: integración de ThemeProvider y StorageProvider al funcionamiento del sistema

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
+// frontend/src/main.jsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from './context/ThemeContext'
+import { StorageProvider } from './context/StorageProvider'
+import App from './App'
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    {/* ThemeProvider envuelve toda la app para que cualquier componente pueda acceder al tema */}
+    <ThemeProvider>
+      {/* StorageProvider envuelve toda la app para que cualquier componente pueda acceder a los datos */}
+      <StorageProvider>
+        <App />
+      </StorageProvider>
+    </ThemeProvider>
+  </StrictMode>
 )
